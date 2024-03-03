98b89d010fb1c7b654760ef2c99a8fb8b344aa20
Farhan Siddiq <157281829+farhansiddiq16@users.noreply.github.com>
2024-03-03 18:13:50 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+
+
+import inquirer from "inquirer"
+
+let num = await inquirer.prompt({
+    type: "number",
+    name: "user",
+    message: "Please enter the amount of seconds"
+})
+
+
+const countdown = (second: number) => {
+    const interval = setInterval(() => {
+        console.log(`${second} seconds remaining`);
+        second--;
+        if (second < 0) {
+            clearInterval(interval);
+            console.log("Countdown is finished");
+        }
+    }, 1000)
+}
+countdown(num.user)
+
